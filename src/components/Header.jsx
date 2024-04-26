8d786274969455305a867e56f2f6573305572be9
Andrey <38962528+Chekavoo@users.noreply.github.com>
2024-04-26 13:28:53 +0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className={'header'}>
+        <img
+            src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.2116175301.1713398400&semt=ais"/>
+    </header>
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className={'content'}>
+        <div>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEzQhMzN0sJnGfqdi2aHD7fzZWirfyYB1DoFc7EQPiUQ&s"
+                alt=""/>
+        </div>
+        <div>
+            <img src="https://th.bing.com/th/id/OIG4.LgUj9FIjzUbdTSMn0mRg" alt=""/>
+        </div>
+        <div>
+            My Post
+            <div>New post</div>
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
+    </div>
+}
+export default Profile;
